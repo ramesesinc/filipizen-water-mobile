3a920815d51431d8fa1da4a502ab2645a6010469
Kevin Abaquita <nivekbusinesses@gmail.com>
2024-05-20 11:45:16 +0800

    Updates: - added clear data button in settings - removed sync bill formula button in settings

--- screens/WaterModule/WaterSettings/WaterSettings.tsx
+++ screens/WaterModule/WaterSettings/WaterSettings.tsx
@@ -1,43 +1,83 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Modal } from 'react-native'
 import { MaterialCommunityIcons, FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 
 
 import { styles } from './styles'
 import WaterHeader from '../../../components/Water/WaterHeader';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SQLITE from 'expo-sqlite'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useState } from 'react';
 
 const WaterSettings = ({ navigation }) => {
+  const db = SQLITE.openDatabase('example.db');
 
+  const [open, setOpen] = useState(false)
 
-  const handleSync = async () => {
-    try {
-      const res = await fetch("http://192.168.2.11:8040/osiris3/json/enterprise/WaterConsumptionFormulaService.getFormula");
-      const data = await res.json();
-      if (data) {
-        await AsyncStorage.setItem('formula', data.formula.toString());
-        alert("Bill formula has been synced")
-      }
-    } catch (e) {
-      alert(`FAILED: ${e}`)
-    }
+  const handleClearData = async () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT IN ('android_metadata', 'sqlite_sequence');`,
+        [],
+        (_, { rows }) => {
+          // Step 2: Iterate over each table and drop it
+          rows._array.forEach(({ name }) => {
+            tx.executeSql(`DROP TABLE IF EXISTS ${name};`, [], 
+              () => {
+                console.log(`Dropped table ${name}`);
+              }, 
+              (_, error) => {
+                console.error(`Error dropping table ${name}:`, error);
+                return true; // signal that an error occurred
+              }
+            );
+          });
+        },
+        (_, error) => {
+          console.error('Error retrieving table names:', error);
+          return true; // signal that an error occurred
+        }
+      );
+    });
+    alert("Data Cleared")
+    setOpen(false)
   }
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
       <WaterHeader navigation={navigation} />
+      {open &&
+        <Modal transparent={true} onRequestClose={() => setOpen(false)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modal}>
+              <Text>Are you sure you want to Clear All Data ?</Text>
+              <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center', justifyContent: 'space-around' }}>
+                <Pressable style={{...styles.save, backgroundColor: 'white'}} onPress={() => {
+                  setOpen(false)
+                }}>
+                  <Text style={{textAlign: 'center'}}>No</Text>
+                </Pressable>
+                <Pressable style={styles.save} onPress={() => handleClearData()}>
+                  <Text style={{textAlign: 'center', color: 'white'}}>Yes</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      }
       <View style={styles.container}>
-        <Pressable style={styles.options} onPress={() => navigation.navigate("Header Settings")}>
+        <TouchableOpacity style={styles.options} onPress={() => navigation.navigate("Header Settings")}>
           <MaterialCommunityIcons name="printer" size={20} color="#00669B" />
           <Text style={styles.optionsText}>Headers Set-up</Text>
-        </Pressable>
-        <Pressable style={styles.options} onPress={handleSync}>
-          <FontAwesome5 name="sync-alt" size={20} color="#00669B" />
-          <Text style={styles.optionsText}>Sync Bill Formula</Text>
-        </Pressable>
-        <Pressable style={styles.options} onPress={() => navigation.navigate("Login")}>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.options} onPress={() => setOpen(true)}>
+        <MaterialCommunityIcons name="archive-remove" size={20} color="#00669B" />
+          <Text style={styles.optionsText}>Clear Data</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.options} onPress={() => navigation.navigate("Login")}>
           <MaterialCommunityIcons name="logout" size={20} color="#00669B" />
           <Text style={styles.optionsText}>Logout</Text>
-        </Pressable>
+        </TouchableOpacity>
       </View>
     </View>
   )
--- screens/WaterModule/WaterSettings/styles.ts
+++ screens/WaterModule/WaterSettings/styles.ts
@@ -19,6 +19,30 @@ export const styles = StyleSheet.create({
     },
     optionsText: {
         fontSize: 20
+    },
+    modalContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)'
+    },
+    modal: {
+        backgroundColor: 'white',
+        height: 200,
+        width: 200,
+        padding: 20,
+        borderRadius: 20,
+        justifyContent: 'space-around'
+    },
+    save: {
+        backgroundColor: "green",
+        padding: 10,
+        width: 70,
+        alignSelf: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'grey'
     }
 
 })
